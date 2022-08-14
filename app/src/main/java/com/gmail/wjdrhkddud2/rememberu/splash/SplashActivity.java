9316da45676743da11c9ac5fa9419ccefddddb5e
IamYeong <wjdrhkddud2@gmail.com>
2022-08-14 18:44:51 +0900

    Add splash activity, splash image

--- app/src/main/java/com/gmail/wjdrhkddud2/rememberu/splash/SplashActivity.java
+++ app/src/main/java/com/gmail/wjdrhkddud2/rememberu/splash/SplashActivity.java
@@ -1,14 +1,16 @@
-package com.gmail.wjdrhkddud2.rememberu;
+package com.gmail.wjdrhkddud2.rememberu.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.gmail.wjdrhkddud2.rememberu.R;
+
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
     }
 }
